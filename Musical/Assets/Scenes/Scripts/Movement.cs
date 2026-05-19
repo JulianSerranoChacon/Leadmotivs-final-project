b8d67a19e37f3664fa288758ac5716bb2418b276
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2026-05-19 17:39:44 +0200

    proyecto de unity

---
+++ Musical/Assets/Scenes/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Floor")
+            isGrounded = true;
+    }
+}
